16808dfa75f5c81a8939a82e2958408a0398d326
Faid <faid@fisip.net>
2019-11-19 20:05:04 +0700

    auto uri

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].str_replace('index.php', '', $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
