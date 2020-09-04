9b4df70e1d4ceedd824a34aaac3cf217073623c8
Faid <faid@fisip.net>
2020-09-04 11:27:43 +0700

    update

--- install.php
+++ install.php
@@ -8,6 +8,7 @@ if ($PNAME) {
 	if (!is_dir($PATH)) {
 		install_exec("mkdir \"$PATH\"");
 		install_exec("cd \"$PATH\"");
+
 		chdir($PATH);
 		$BASENAME = basename(__DIR__);
 
@@ -15,19 +16,34 @@ if ($PNAME) {
 		install_exec("ln -s ../".$BASENAME."/.htaccess ./");
 		install_exec("ln -s ../".$BASENAME."/index.php ./");
 		install_exec("ln -s ../".$BASENAME."/system ./");
+
 		install_exec("mkdir application");
+
 		install_exec("mkdir application/cache");
+		install_exec("ln -s ../../".$BASENAME."/application/cache/index.html ./application/cache/");
+
 		install_exec("mkdir application/config");
+		
 		install_exec("mkdir application/controllers");
+		
 		install_exec("mkdir application/core");
+		
 		install_exec("mkdir application/helpers");
+		
 		install_exec("mkdir application/hooks");
+		
 		install_exec("mkdir application/language");
+		
 		install_exec("mkdir application/libraries");
+		
 		install_exec("mkdir application/logs");
+		
 		install_exec("mkdir application/models");
+		
 		install_exec("mkdir application/third_party");
+		
 		install_exec("mkdir application/views");
+		
 		install_exec("ln -s ../../".$BASENAME."/application/.htaccess ./application/");
 		install_exec("ln -s ../../".$BASENAME."/application/index.html ./application/");
 	}else{
