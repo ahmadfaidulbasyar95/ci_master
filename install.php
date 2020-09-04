0f09e0364c51feffa8ad376dad35ef3896379fbc
Faid <faid@fisip.net>
2020-09-04 18:38:44 +0700

    update

--- install.php
+++ install.php
@@ -23,26 +23,52 @@ if ($PNAME) {
 		install_exec("ln -s ../../../".$BASENAME."/application/cache/index.html ./application/cache/");
 
 		install_exec("mkdir application/config");
+		install_exec("cp ../".$BASENAME."/application/config/* ./application/config/");
 		
 		install_exec("mkdir application/controllers");
+		install_exec("ln -s ../../../".$BASENAME."/application/controllers/index.html ./application/controllers/");
+		install_exec("ln -s ../../../".$BASENAME."/application/controllers/_Pea.php ./application/controllers/");
+		install_exec("ln -s ../../../".$BASENAME."/application/controllers/Welcome.php ./application/controllers/");
 		
 		install_exec("mkdir application/core");
+		install_exec("ln -s ../../../".$BASENAME."/application/core/index.html ./application/core/");
 		
 		install_exec("mkdir application/helpers");
+		install_exec("ln -s ../../../".$BASENAME."/application/helpers/index.html ./application/helpers/");
 		
 		install_exec("mkdir application/hooks");
+		install_exec("ln -s ../../../".$BASENAME."/application/hooks/index.html ./application/hooks/");
 		
 		install_exec("mkdir application/language");
+		install_exec("ln -s ../../../".$BASENAME."/application/language/index.html ./application/language/");
+		install_exec("ln -s ../../../".$BASENAME."/application/language/english ./application/language/");
 		
 		install_exec("mkdir application/libraries");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/index.html ./application/libraries/");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/excel ./application/libraries/");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/pdf ./application/libraries/");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/pea ./application/libraries/");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/output.php ./application/libraries/");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/pagination.php ./application/libraries/");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/path.php ./application/libraries/");
+		install_exec("ln -s ../../../".$BASENAME."/application/libraries/table.php ./application/libraries/");
 		
 		install_exec("mkdir application/logs");
+		install_exec("ln -s ../../../".$BASENAME."/application/logs/index.html ./application/logs/");
 		
 		install_exec("mkdir application/models");
+		install_exec("ln -s ../../../".$BASENAME."/application/models/index.html ./application/models/");
+		install_exec("ln -s ../../../".$BASENAME."/application/models/_db_model.php ./application/models/");
+		install_exec("ln -s ../../../".$BASENAME."/application/models/_encrypt_model.php ./application/models/");
+		install_exec("ln -s ../../../".$BASENAME."/application/models/_pea_model.php ./application/models/");
 		
 		install_exec("mkdir application/third_party");
+		install_exec("ln -s ../../../".$BASENAME."/application/third_party/index.html ./application/third_party/");
 		
 		install_exec("mkdir application/views");
+		install_exec("ln -s ../../../".$BASENAME."/application/views/index.html ./application/views/");
+		install_exec("ln -s ../../../".$BASENAME."/application/views/welcome_message.php ./application/views/");
+		install_exec("ln -s ../../../".$BASENAME."/application/views/errors ./application/views/");
 		
 		install_exec("ln -s ../../".$BASENAME."/application/.htaccess ./application/");
 		install_exec("ln -s ../../".$BASENAME."/application/index.html ./application/");
