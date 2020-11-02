c1b8e2520d100805c51c566e66e6d8e55c179fee
Faid <faid@fisip.net>
2020-11-02 12:41:31 +0700

    up

--- application/controllers/admin/Location.php
+++ application/controllers/admin/Location.php
@@ -21,16 +21,12 @@ class Location extends CI_Controller
 
 		$form->initRoll('WHERE `par_id`='.$par_id.' ORDER BY `title` ASC');
 
-		$form->roll->setHeader('Data Lokasi');
-
 		$form->roll->addInput('title', 'sqllinks');
 		$form->roll->input->title->setTitle('Nama');
 		$form->roll->input->title->setLinks('admin/location/form');
-
-		$form->roll->addInput('detail', 'sqlplaintext');
-		$form->roll->input->detail->setTitle('Deskripsi');
-		$form->roll->input->detail->setDisplayColumn(1);
 		
+		$form->roll->setDeleteTool(false);
+		$form->roll->setSaveTool(false);
 		$form->roll->addReportAll();
 		$form->roll->action();
 		echo $form->roll->getForm();
@@ -48,9 +44,7 @@ class Location extends CI_Controller
 		
 		$form->edit->addInput('title','text');
 		$form->edit->input->title->setTitle('Nama');
-		
-		$form->edit->addInput('detail','text');
-		$form->edit->input->detail->setTitle('Deskripsi');
+		$form->edit->input->title->setRequire();
 		
 		$form->edit->action();
 		echo $form->edit->getForm();
