9c3d6d922adcc29f4a4802d8bb441924dde4241b
Faid <faid@fisip.net>
2020-11-11 17:51:14 +0700

    up

--- application/views/admin/includes/js/script_additional.js
+++ application/views/admin/includes/js/script_additional.js
@@ -1,5 +1,60 @@
 (function() {
 	window.addEventListener('load', function() { 
+		function _iframe_resize(el, width = 0, height = 0) {
+			var fr = el.find('._iframe_body');
+			if (width == 0 || height == 0) {
+				fr.css('height', '');
+				el.css({
+					width : '',
+					top : 0,
+					left : 0
+				}).resizable({
+					disabled: true
+				}).draggable({
+					drag: function( event, ui ) {
+						ui.position.left = 0;
+						ui.position.top = 0;
+					}
+				});
+			}else{
+				fr.css('height', height - 32);
+				el.css('width', width).resizable({
+					disabled: false,
+					resize: function( event, ui ) {
+						var width_offside = w_width - ui.position.left - 5;
+						if (ui.size.width > width_offside) {
+							ui.size.width = width_offside;
+						}
+						var height_offside = w_height - ui.position.top - 35;
+						if (ui.size.height > height_offside) {
+							ui.size.height = height_offside;
+						}
+						fr.css('height', ui.size.height - 32);
+						el.data('width', ui.size.width);
+						el.data('height', ui.size.height);
+					}
+				}).draggable({
+					drag: function( event, ui ) {
+						if(ui.position.left < 0) {
+							ui.position.left = 0;
+						}
+						if(ui.position.top < 0) {
+							ui.position.top = 0;
+						}
+						var left_offside = w_width - el.data('width') - 5;
+						if(ui.position.left > left_offside) {
+							ui.position.left = left_offside;
+						}
+						var top_offside = w_height - el.data('height') - 35;
+						if(ui.position.top > top_offside) {
+							ui.position.top = top_offside;
+						}
+					}
+				});
+			}
+		}
+		var w_width      = $(window).width();
+		var w_height     = $(window).height();
 		var z_index_last = 1;
 		$('#_start').on('click', function(event) {
 			event.preventDefault();
@@ -11,7 +66,19 @@
 		$('body').on('click', '._iframe_maximize', function(event) {
 			event.preventDefault();
 			$(this).children('.fa').toggleClass('fa-window-maximize').toggleClass('fa-window-restore');
-			$(this).parents('._iframes').toggleClass('_iframe_move');
+			var el = $(this).parents('._iframes');
+			el.toggleClass('_iframe_move');
+			if (el.data('width') == undefined) {
+				el.data('width', w_width/2);
+			}
+			if (el.data('height') == undefined) {
+				el.data('height', w_height/2);
+			}
+			if (el.hasClass('_iframe_move')) {
+				_iframe_resize(el, el.data('width'), el.data('height'));
+			}else{
+				_iframe_resize(el, 0, 0);
+			}
 		});
 		$('body').on('click', '._iframes', function(event) {
 			$('._iframe_active').removeClass('_iframe_active');
@@ -35,6 +102,15 @@
 			event.preventDefault();
 			var el = $(this).parents('._iframes');
 			el.toggle(300).toggleClass('_iframe_hide');
+		});
+		$('body').on('click', '._iframe_close', function(event) {
+			event.preventDefault();
+			var el = $(this).parents('._iframes');
+			el.fadeOut(300);
+			$('._iframe_toggles[data-id="'+el.data('id')+'"]').remove();
+			setTimeout(function() {
+				el.remove();
+			}, 300);
 		});
 	}, false);
 })();
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -7,10 +7,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<?php
 		$tpl->css('includes/libraries/bootstrap-3.4.1-dist/css/bootstrap.min.css');
 		$tpl->css('includes/libraries/font-awesome-4.7.0/css/font-awesome.min.css');
+		$tpl->css('includes/libraries/jquery-ui-1.12.1/jquery-ui.min.css');
 		$tpl->css('includes/css/style_additional.css');
 
 		$tpl->js('includes/libraries/jquery-3.5.1/jquery.min.js');
 		$tpl->js('includes/libraries/bootstrap-3.4.1-dist/js/bootstrap.min.js');
+		$tpl->js('includes/libraries/jquery-ui-1.12.1/jquery-ui.min.js');
 		$tpl->js('includes/js/script_additional.js');
 		?>
 	</head>
