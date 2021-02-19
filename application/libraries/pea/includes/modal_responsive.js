8144018c7dab9fa26e84b2449a4fb91c6e27d75e
Faid <faid@fisip.net>
2021-02-19 14:12:17 +0700

    up

--- application/libraries/pea/includes/modal_responsive.js
+++ application/libraries/pea/includes/modal_responsive.js
@@ -2,6 +2,7 @@
 	window.addEventListener('load', function() { 
 		var x = $('#modal-modal_processing', window.parent.document);
 		if (x.length) {
+			$('.form_pea_edit').children('.panel').css('margin-bottom', '0');
 			setTimeout(function() {
 				x.find('iframe').height($('html').height());
 			}, 100);
