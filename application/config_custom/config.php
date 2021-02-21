55a4b495357563ac10fc9b86158f6e9a5750251f
Faid <faid@fisip.net>
2021-02-21 19:38:41 +0700

    up

---
+++ application/config_custom/config.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$c_site = FCPATH.'files/cache/config/site.cfg';
+if (is_file($c_site)) {
+	$c_site = json_decode(file_get_contents($c_site), 1);
+	if ($c_site) {
+		$config['base_url'] = $_SERVER['REQUEST_SCHEME'].'://'.$c_site['domain'].str_replace('index.php', '', $_SERVER['SCRIPT_NAME']);
+	}
+}
