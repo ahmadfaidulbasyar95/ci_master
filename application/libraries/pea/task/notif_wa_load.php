f80dd1f7acb1e3f4c11a0f5cabaef9781305d375
Faid <faid@fisip.net>
2021-06-29 20:17:53 +0700

    up

--- application/libraries/pea/task/notif_wa_load.php
+++ application/libraries/pea/task/notif_wa_load.php
@@ -5,8 +5,43 @@ include_once __DIR__.'/../../output.php';
 
 lib_output_json(array(
 	array(
-		// 'phone' => '6282327064384',
 		'phone' => '6282142538006',
-		'text'  => "WA BOT\nHello World"
+		'text'  => "WA BOT\nHello World 1"
+	),
+	array(
+		'phone' => '6282327064384',
+		'text'  => "WA BOT\nHello World 2"
+	),
+	array(
+		'phone' => '6282142538006',
+		'text'  => "WA BOT\nHello World 3"
+	),
+	array(
+		'phone' => '6282327064384',
+		'text'  => "WA BOT\nHello World 4"
+	),
+	array(
+		'phone' => '6282142538006',
+		'text'  => "WA BOT\nHello World 5"
+	),
+	array(
+		'phone' => '6282327064384',
+		'text'  => "WA BOT\nHello World 6"
+	),
+	array(
+		'phone' => '6282142538006',
+		'text'  => "WA BOT\nHello World 7"
+	),
+	array(
+		'phone' => '6282327064384',
+		'text'  => "WA BOT\nHello World 8"
+	),
+	array(
+		'phone' => '6282142538006',
+		'text'  => "WA BOT\nHello World 9"
+	),
+	array(
+		'phone' => '6282327064384',
+		'text'  => "WA BOT\nHello World 10"
 	),
 ));
