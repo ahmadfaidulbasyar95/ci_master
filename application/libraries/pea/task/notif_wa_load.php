e1d11a46a0da7268fc1deddd1ce7ca70a9a16ba3
Faid <faid@fisip.net>
2021-06-29 19:41:41 +0700

    up

---
+++ application/libraries/pea/task/notif_wa_load.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+include_once __DIR__.'/../../output.php';
+
+lib_output_json(array(
+	array(
+		// 'phone' => '6282327064384',
+		'phone' => '6282142538006',
+		'text'  => "WA BOT\nHello World"
+	),
+));
