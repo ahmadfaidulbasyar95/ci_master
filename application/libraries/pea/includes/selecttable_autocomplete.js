4df4c4b9f5c103ca82ebd15c40f899da868e76da
Faid <faid@fisip.net>
2021-07-22 12:38:47 +0700

    up

---
+++ application/libraries/pea/includes/selecttable_autocomplete.js
@@ -0,0 +1,98 @@
+(function() {
+	window.addEventListener('load', function() { 
+		if (window.getOptionNested  == undefined) {
+			window.getOptionNested = function(option = [], nested = 0, prefix = '') {
+				var option_ = [];
+				var prefix_ = (prefix) ? prefix+' ' : '';
+				$.each(option, function(key, value) {
+					if (value.nested == nested) {
+						value.key = prefix_+value.key;
+						option_.push(value);
+						$.each(window.getOptionNested(option, value.value, prefix_+'->'), function(index, value_) {
+							option_.push(value_);
+						});
+					}
+				});
+				return option_;
+			}
+		}	
+		$('.selecttable_autocomplete').each(function(index, el) {
+			var elem = $(this);
+			if (!elem.data('selecttable_autocomplete_load')) {
+				elem.data('selecttable_autocomplete_load', 1);
+				var value      = elem.data('value');
+				var token      = elem.data('token');
+				var dependent  = elem.data('dependent');
+				var nested     = elem.data('nested');
+				var option_def = [];
+				var multiple   = (elem.attr('multiple') != undefined) ? 1 : 0;
+				var required   = (elem.attr('required') != undefined) ? 'required="required"' : '';
+
+				elem.children('option').each(function(index, el) {
+					el = $(this);
+					option_def.push({
+						'value':    el.attr('value'),
+						'key':      el.html(),
+						'selected': (el.attr('selected')) ? 1 : 0
+					});
+				});
+				
+				$('<input type="text" class="form-control selecttable_autocomplete_input" value="" '+required+' placeholder="'+elem.attr('title')+'"><div class="selecttable_autocomplete_result"></div>').insertAfter(elem);
+				
+				var input  = elem.next('.selecttable_autocomplete_input');
+				var result = input.next('.selecttable_autocomplete_result');
+
+				if (dependent != undefined) {
+					dependent = $('[name="'+dependent+'"]');
+					dependent.on('change', function(event) {
+						input.val('').trigger('keyup');
+					});
+				}
+
+				input.on('keyup', function(event) {
+					var s = input.val(); 
+					if (s) {
+						var v = '0'; 
+						if (dependent != undefined) {
+							v = dependent.val(); 
+						}
+						$.ajax({
+							url: _URL+'_T/getdata',
+							type: 'POST',
+							dataType: 'json',
+							data: {token: token, v: v, s: s},
+						})
+						.done(function(out) {
+							if (nested) {
+								out = window.getOptionNested(out);
+							}
+							var option = option_def;
+							$.each(out, function(index, val) {
+								if (multiple) {
+									if ($.inArray(val.value, value) != -1) {
+										val.selected = ' selected';
+									}else{
+										val.selected = '';
+									}
+								}else{
+									if (value == val.value) {
+										val.selected = ' selected';
+									}else{
+										val.selected = '';
+									}
+								}
+								option += '<option value="'+val.value+'"'+val.selected+'>'+val.key+'</option>';
+							});
+							elem.html(option).trigger('change');
+						})
+						.fail(function() {
+							alert("Something Wrong. Please try again later or reload this page !");
+						});
+					}else{
+						elem.html('').trigger('change');
+					}
+				}).trigger('change');
+			}
+		});
+	}, false);
+})();
